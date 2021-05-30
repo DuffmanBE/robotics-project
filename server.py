d6e1a50996d5e97975a9e2a57b790c9491c93fbf
Simon Van de Water <simon.vandewater@gmail.com>
2021-05-30 17:56:57 +0200

    initial commit

---
+++ server.py
@@ -0,0 +1,36 @@
+import websockets
+import pickle
+import struct
+from sense_hat import SenseHat
+
+
+def recv(s):
+	data = s.recv(4, socket.MSG_WAITALL)
+	data_len = struct.unpack('>i', data)[0]
+	data = pickle.loads(s.recv(data_len, socket.MSG_WAITALL))
+	print("Received data!", data)
+	if 'action' in data:
+		if data["action"] == "set_pixel":
+			coordinates = data["payload"]
+			sense.clear()
+			sense.set_pixel(coordinates[0], coordinates[1], 255, 0, 0)
+	return data
+
+# Create server
+
+sense = SenseHat()
+sense.clear()
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+s.bind(('0.0.0.0', 9395))
+s.listen(10)
+
+
+while True:
+	# Accept a client
+	print("Hello world!")
+	conn, addr = s.accept()
+	while True:
+		recv(conn)
+
+
